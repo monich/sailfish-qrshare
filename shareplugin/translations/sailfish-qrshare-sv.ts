ac0427e0591d485b0661f50e4cfb4fb6eb07845e
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-05-16 06:17:50 +0200

    Create sailfish-qrshare-sv.ts
    Here's Swedish translation.

---
+++ shareplugin/translations/sailfish-qrshare-sv.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="qrshare-display_name">
+        <source>QR Share</source>
+        <extracomment>Plugin display name</extracomment>
+        <translation>Generera QR-kod</translation>
+    </message>
+    <message id="qrshare-menu-save_to_gallery">
+        <source>Save to Gallery</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Spara i Galleri</translation>
+    </message>
+    <message id="qrshare-placeholder-text_too_long">
+        <source>Text is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Texten är för lång för QR-kod.</translation>
+    </message>
+    <message id="qrshare-placeholder-no_text">
+        <source>Nothing to share.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Inget att dela.</translation>
+    </message>
+</context>
+</TS>
