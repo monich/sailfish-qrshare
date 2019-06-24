aa59874bb6accc62478219891cad4372cedeaa2f
Quentí <33203663+Quenty31@users.noreply.github.com>
2019-06-24 17:37:15 +0200

    French translation

---
+++ shareplugin/translations/sailfish-qrshare-fr.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name></name>
+    <message id="qrshare-display_name">
+        <source>QR Share</source>
+        <extracomment>Plugin display name</extracomment>
+        <translation>Générer un QR code</translation>
+    </message>
+    <message id="qrshare-menu-save_to_gallery">
+        <source>Save to Gallery</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Enregistrer dans la galerie</translation>
+    </message>
+    <message id="qrshare-placeholder-no_text">
+        <source>Nothing to share.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Rien à partager.</translation>
+    </message>
+    <message id="qrshare-placeholder-qrcode-too_long">
+        <source>Text is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Le texte est trop long pour un QR code.</translation>
+    </message>
+    <message id="qrshare-placeholder-aztec-too_long">
+        <source>Text is too long for Aztec code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Le texte est trop long pour un code Aztec.</translation>
+    </message>
+</context>
+</TS>
